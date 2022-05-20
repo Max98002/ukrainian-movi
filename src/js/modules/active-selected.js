1342a5102ce5239f8c2083c7f81aa86e3da87234
Maksym <bilanych.max98009@gmail.com>
2022-05-20 11:55:18 +0300

    fix menu

--- src/js/modules/active-selected.js
+++ src/js/modules/active-selected.js
@@ -1,25 +1,34 @@
 const activeSelected = () => {
   const btns = document.querySelectorAll('.selected-blocks__btn');
 
-  btns.forEach(btn => {
-    btn.addEventListener('focus', function() {
-      const menu = btn.nextElementSibling;
+  function addStyle(i) {
+    btns[i].classList.add('selected-blocks__btn_active');
+    btns[i].nextElementSibling.classList.add('selected-blocks-list_active');
+  }
 
-      this.classList.add('selected-blocks__btn_active');
-      menu.classList.add('selected-blocks-list_active');
+  function removeStyle() {
+    for (let i = 0; i < btns.length; i++) {
+      btns[i].classList.remove('selected-blocks__btn_active');
+      btns[i].nextElementSibling.classList.remove('selected-blocks-list_active');
+    }
+  }
 
-    });
-  });
+  btns.forEach((btn, i) => {
+    btn.addEventListener('click', (e) => {
+      const target = e.target;
 
-  btns.forEach(btn => {
-    btn.addEventListener('blur', function() {
-      const menu = btn.nextElementSibling;
+      removeStyle();
+      addStyle(i);
+    })
+  })
 
-      this.classList.remove('selected-blocks__btn_active');
-      menu.classList.remove('selected-blocks-list_active');
+  window.addEventListener('click', (e) => {
+    const target = e.target;
 
-    });
-  });
+    if (target && !target.matches('.selected-blocks__btn')) {
+      removeStyle();
+    }
+  })
   
 };
 
