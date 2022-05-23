92f66c2adb8108ddd535342d76828e648c3bc856
Maksym <bilanych.max98009@gmail.com>
2022-05-23 13:51:09 +0300

    add fix selected menu

--- src/js/modules/active-selected.js
+++ src/js/modules/active-selected.js
@@ -51,6 +51,42 @@ const activeSelected = () => {
       }
     }
   });
+
+  window.addEventListener('touchstart', (e) => {
+    const target = e.target;
+
+    if (target && !target.matches('.selected-blocks__btn') && window.screen.availWidth < 820) {
+      for (let i = 0; i < btns.length; i++) {
+        btns[i].classList.remove('selected-blocks__btn_active');
+        btns[i].nextElementSibling.classList.remove('selected-blocks-list_active');
+        intervalId = setTimeout(() => {
+          btns[i].classList.remove('open');
+        }, 0);
+      }
+    }
+  });
+
+  // function addStyle(i) {
+  //   btns[i].classList.add('selected-blocks__btn_active');
+  //   btns[i].nextElementSibling.classList.add('selected-blocks-list_active');
+  // }
+
+  // function removeStyle() {
+  //   for (let i = 0; i < btns.length; i++) {
+  //     btns[i].classList.remove('selected-blocks__btn_active');
+  //     btns[i].nextElementSibling.classList.remove('selected-blocks-list_active');
+  //   }
+  // }
+
+  // if (window.screen.availWidth > 820) {
+  //   btns.forEach((btn, i) => {
+  //     btn.addEventListener('click', function () {
+  //       removeStyle();
+  //       addStyle(i);
+  //     });
+  //   })
+  // }
+
 };
 
 export default activeSelected;
